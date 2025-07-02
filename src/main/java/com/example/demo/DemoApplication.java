10f664b96c2bf15ce5d97672f969019a51e977cc
Stephen Matula <smatula@redhat.com>
2025-07-02 10:08:11 -0400

    Test PR from TSSC e2e test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World - Updated! 1751465288434 !";
     }
 
     public static void main(String[] args) {
